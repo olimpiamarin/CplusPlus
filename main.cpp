832a3a841d9b4582a8ef6f580254425afe0b9cf5
olimpiamarin <olimpiamarin84@gmail.com>
2017-02-23 02:40:03 +0200

    Curs 1

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i, j;
+    i = 2; j = 2;
+    int sum = i + j;
+    cout << "Suma = " << sum << endl;
+    //cin.get();
+    return 0;
+}
